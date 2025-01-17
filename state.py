9fa4b427f4112baef8c39b3a5941bafb8088f2f8
TNPKpenguin <tatsanasrisawang2@gmail.com>
2025-01-17 10:20:28 +0700

    add state information

--- state.py
+++ state.py
@@ -1,3 +1,5 @@
 class State:
-    def __init__():
-        pass
+    def __init__(self, n_grass, n_rabbit, n_wolf):
+        self.n_grass = n_grass
+        self.n_rabbit = n_rabbit
+        self.n_wolf = n_wolf
